57e8235edc2affa3288161100b8f5656e357b8e8
Jordan <210195996+jvrdvn@users.noreply.github.com>
2026-06-28 22:51:17 -0700

    Day 2: Type() and isinstance()

---
+++ type and isinstance.py
@@ -0,0 +1,18 @@
+my_integer_var = 10
+print(type(my_integer_var))
+
+my_float_var = 3.5
+print(type(my_float_var))
+
+my_string_var = 'My name is'
+print(type(my_string_var))
+
+my_boolean_var = True
+print(type(my_boolean_var))
+
+my_set_var = {7, 'Hello', 9.0}
+print(type(my_set_var))
+
+my_dictionary_var = {'name': 'Alice', 'age': 25}
+print(type(my_dictionary_var))
+
