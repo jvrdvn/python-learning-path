ef22490bd4d8684ed43cbef064075f74f96e17e9
Jordan <210195996+jvrdvn@users.noreply.github.com>
2026-06-28 22:44:39 -0700

    Day 1: Python data types

---
+++ Data Types.py
@@ -0,0 +1,31 @@
+my_integer_var = 10
+print('integer:', my_integer_var)
+
+my_float_var = 9.8
+print('float:', my_float_var)
+
+my_string_var = 'hello'
+print('string:', my_string_var)
+
+my_boolean_var = True
+print('boolean:', my_boolean_var)
+
+my_set_var = {7, 'hello', 9.5}
+print('set:', my_set_var)
+
+my_dictionary_var = {'name':'John Doe', 'age': 25}
+print('dictionary:', my_dictionary_var)
+
+my_tuple_var = (7, 'hello', 8.5)
+print('tuple:', my_tuple_var)
+
+my_range_var = range(5)
+print('range:', my_range_var)
+
+my_list = [22, 'hello world', 3.14, True]
+print(my_list)
+
+my_none_var = None
+print('None:', my_none_var)
+
+
